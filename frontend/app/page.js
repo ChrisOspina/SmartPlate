1488d5c717d2bca8e39b38f364dc806b31467873
ChrisOspina <ChrisOspina99@gmail.com>
2026-02-04 21:53:20 -0500

    Scaffolding commit

---
+++ frontend/app/page.js
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      Dummy Page
+      <Button variant="primary" size="lg">
+        Click Me
+      </Button>
+    </div>
+  );
+}
