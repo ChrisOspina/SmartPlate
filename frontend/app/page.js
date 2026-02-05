80ae9738c8dd2cccbb39d4454762d4f493e28a7a
ChrisOspina <ChrisOspina99@gmail.com>
2026-02-05 15:51:43 -0500

    Managing subscription and  billing

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,11 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { PricingTable } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className=" bg-stone-50 min-h-screen text-stone-900">
       <section className="pt-32 pb-20 px-4">
-        Dummy Page
         <Button variant="primary" size="lg">
           Click Me
         </Button>
